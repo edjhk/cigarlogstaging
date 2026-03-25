c9f3c891ac6c36296ed3d09e30469379729d30b0
edjhk <jacobson.edward@gmail.com>
2026-03-25 11:38:18 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,2 @@
-const CACHE = 'cigarlog-v1';
-const ASSETS = [
-  './index.html',
-  './cigar_brands.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
